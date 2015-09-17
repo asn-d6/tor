a110f0cdc77ed66bee128ab60efc8631b10b581c
George Kadianakis <desnacked@riseup.net>
2015-09-17 17:00:11 +0300

    Delete some unused code to make it compile.

--- src/or/shared-random.c
+++ src/or/shared-random.c
@@ -9,82 +9,8 @@
 
 #include "shared-random.h"
 
-/* String representation of a protocol phase. */
-static const char *phase_str[] = { "commit", "reveal" };
-
-/* String representation of a shared random value status. */
-static const char *srv_status_str[] = { "fresh", "non-fresh" };
-
 /* Default filename of the shared random state on disk. */
 static const char *default_fname = "sr-state";
-/* Constant static seed of the shared random value. */
-static const char *srv_seed = "shared-random";
-/* Disaster shared random value seed. */
-static const char *disaster_seed = "shared-random-disaster";
-
-/* Return a string representation of a srv status. */
-static const char *
-get_srv_status_str(sr_srv_status_t status)
-{
-  switch (status) {
-  case SR_SRV_STATUS_FRESH:
-  case SR_SRV_STATUS_NONFRESH:
-    return srv_status_str[status];
-  default:
-    /* Unknown status shouldn't be possible. */
-    tor_assert(0);
-  }
-}
-
-/* Return a string representation of a protocol phase. */
-static const char *
-get_phase_str(sr_phase_t phase)
-{
-  switch (phase) {
-  case SR_PHASE_COMMIT:
-  case SR_PHASE_REVEAL:
-    return phase_str[phase];
-  default:
-    /* Unknown phase shouldn't be possible. */
-    tor_assert(0);
-  }
-}
-
-/* Return a phase value from a name string. */
-static sr_phase_t
-get_phase_from_str(const char *name)
-{
-  int i;
-  sr_phase_t phase = -1;
-
-  tor_assert(name);
-
-  for (i = 0; i < ARRAY_LENGTH(phase_str); i++) {
-    if (!strcmp(name, phase_str[i])) {
-      phase = i;
-      break;
-    }
-  }
-  return phase;
-}
-
-/* Return a status value from a string. */
-static sr_srv_status_t
-get_status_from_str(const char *name)
-{
-  int i;
-  sr_srv_status_t status;
-
-  tor_assert(name);
-
-  for (i = 0; i < ARRAY_LENGTH(srv_status_str); i++) {
-    if (!strcmp(name, srv_status_str[i])) {
-      status = i;
-      break;
-    }
-  }
-  return status;
-}
 
 /* Allocate a new commit object and initializing it with <b>identity</b>
  * that MUST be provided. The digest algorithm is set to the default one
