20da0beb9c915714b7a7156ef2276db9d368b0cb
David Goulet <dgoulet@ev0ke.net>
2015-11-25 11:24:53 -0500

    Add torrc option to enable/disable SR voting
    Signed-off-by: David Goulet <dgoulet@ev0ke.net>

--- src/or/or.h
+++ src/or/or.h
@@ -4461,6 +4461,12 @@ typedef struct {
 
   /** Autobool: Do we try to retain capabilities if we can? */
   int KeepBindCapabilities;
+
+  /** Bool (default: 1): Switch for the shared random protocol. Only
+   * relevant to a directory authority. If off, the authority won't
+   * participate in the protocol. If on (default), a flag is added to the
+   * vote indicating participation. */
+  int VoteSharedRandom;
 } or_options_t;
 
 /** Persistent state for an onion router, as saved to disk. */
