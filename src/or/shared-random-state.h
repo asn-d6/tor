3802d64b66eacb078adb9eb6c964ff26b2e5825c
David Goulet <dgoulet@ev0ke.net>
2015-11-26 15:24:26 -0500

    Make a function static
    Signed-off-by: David Goulet <dgoulet@ev0ke.net>

--- src/or/shared-random-state.h
+++ src/or/shared-random-state.h
@@ -83,7 +83,6 @@ sr_srv_t *sr_state_get_previous_srv(void);
 sr_srv_t *sr_state_get_current_srv(void);
 void sr_state_set_previous_srv(sr_srv_t *srv);
 void sr_state_set_current_srv(sr_srv_t *srv);
-void sr_state_rotate_srv(void);
 
 digestmap_t *sr_state_get_commits(void);
 sr_commit_t *sr_state_get_commit_by_rsa(const char *rsa_fpr);
