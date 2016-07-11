36d74a28051f082fbdeff5e364589c10d5fa6a92
George Kadianakis <desnacked@riseup.net>
2016-07-11 19:02:27 +0300

    temp: massaging hs_service.c

--- src/or/hs_service.c
+++ src/or/hs_service.c
@@ -7,62 +7,35 @@
  * \brief Implement next generation service functionality
  **/
 
-#if 0
+#include "hs_establish_intro.h"
 
-/** Our circuit to the intro point just opened! Send out an ESTABLISH_INTRO cell. */
-void
-hs_service_intro_has_opened(origin_circuit_t *circuit)
+static int
+get_establish_intro_payload(uint8_t *payload, uint8_t payload_len,
+                            const hs_establish_intro_cell_t *cell)
 {
-  // Ensure this is for establish intro
-  tor_assert(circuit->base_.purpose == CIRCUIT_PURPOSE_S_ESTABLISH_INTRO);
-  rend_service_t *service = rend_service_get_by_pk_digest(
-                circuit->rend_data->rend_pk_digest);
-  if (!service) {
-    log_warn(LD_REND, "Unrecognized service ID.");
-    return circuit_mark_for_close(TO_CIRCUIT(circuit), END_CIRC_REASON_NOSUCHSERVICE);
+  // Finally, get a binary string and encode the cell
+  int len = 1 + 1 + auth_key_len + 1 + handshake_len + 1 + sig_len;
+  uint8_t buf[len];
+  ssize_t bytes_used = hs_establish_intro_cell_encode(buf, len, cell);
+  if (bytes_used < 0) {
+    log_warn(LD_BUG, "Unable to generate valid ESTABLISH_INTRO cell");
+    return circuit_mark_for_close(TO_CIRCUIT(circuit), END_CIRC_REASON_INTERNAL);
   }
 
-  /* XXX what is this? */
-  if ((count_intro_point_circuits(service) -
-       smartlist_len(service->expiring_nodes)) >
-      service->n_intro_points_wanted) {
-    /* Remove the intro point associated with this circuit, it's being
-     * repurposed or closed thus cleanup memory. */
-    rend_intro_point_t *intro = find_intro_point(circuit);
-    if (intro != NULL) {
-      smartlist_remove(service->intro_nodes, intro);
-      rend_intro_point_free(intro);
-    }
+  // Double check for truncation
+  tor_assert(bytes_used == len);
+}
 
-    if (get_options()->ExcludeNodes) {
-      circuit_mark_for_close(TO_CIRCUIT(circuit), END_CIRC_REASON_NONE);
-    } else {
-      tor_assert(circuit->build_state->is_internal);
-      circuit_change_purpose(TO_CIRCUIT(circuit), CIRCUIT_PURPOSE_C_GENERAL);
-      {
-        rend_data_t *rend_data = circuit->rend_data;
-        circuit->rend_data = NULL;
-        rend_data_free(rend_data);
-      }
-      {
-        crypto_pk_t *intro_key = circuit->intro_key;
-        circuit->intro_key = NULL;
-        crypto_pk_free(intro_key);
-      }
-
-      circuit_has_opened(circuit);
-    }
-    return;
-  }
-
+static hs_establish_intro_cell_t
+generate_establish_intro_cell(void);
+{
   // Generate short-term keypair for use in ESTABLISH_INTRO
   ed25519_keypair_t key_struct;
   if(ed25519_keypair_generate(&key_struct, 0) < 0) {
       return circuit_mark_for_close(TO_CIRCUIT(circuit), END_CIRC_REASON_NONE);
   }
 
-  // Create empty establish_intro cell
-  hs_establish_intro_cell_t *cell = rend_establish_intro_new();
+  hs_establish_intro_cell_t *cell = hs_establish_intro_cell_new();
 
   // Set AUTH_KEY_TYPE: 2 means ed25519
   hs_establish_intro_cell_set_auth_key_type(cell, 2);
@@ -115,20 +88,22 @@ hs_service_intro_has_opened(origin_circuit_t *circuit)
     hs_establish_intro_cell_getarray_sig(cell);
   memcpy(sig_ptr, sig_struct.sig, sig_len);
 
-  // Finally, get a binary string and encode the cell
-  int len = 1 + 1 + auth_key_len + 1 + handshake_len + 1 + sig_len;
-  uint8_t buf[len];
-  ssize_t bytes_used = hs_establish_intro_cell_encode(buf, len, cell);
-  if (bytes_used < 0) {
-    log_warn(LD_BUG, "Unable to generate valid ESTABLISH_INTRO cell");
-    return circuit_mark_for_close(TO_CIRCUIT(circuit), END_CIRC_REASON_INTERNAL);
-  }
+  return cell;
+}
 
-  // Double check for truncation
-  tor_assert(bytes_used == len);
+static void
+send_establish_intro_cell(origin_circuit_t *circuit)
+{
+  /* Get a populated ESTABLISH_INTRO cell */
+  hs_establish_intro_cell_t *cell = generate_establish_intro_cell();
 
-  // Free the cell object and send the message
-  hs_establish_intro_cell_free(cell);
+  /* Get the ESTABLISH_INTRO payload */
+  uint8_t *cell_payload = get_establish_intro_payload(cell);
+
+  // Free the cell object
+  hs_establish_intro_cell_free(cell); /* XXX don't free here */
+
+  /* Send the cell to the network! */
   if (relay_send_command_from_edge(0, TO_CIRCUIT(circuit),
                                    RELAY_COMMAND_ESTABLISH_INTRO,
                                    (const char *)buf, len, circuit->cpath->prev)<0) {
@@ -136,8 +111,67 @@ hs_service_intro_has_opened(origin_circuit_t *circuit)
     return circuit_mark_for_close(TO_CIRCUIT(circuit), END_CIRC_REASON_INTERNAL);
   }
 
+  return 0;
+}
+
+/** Our circuit to the intro point just opened! Send out an ESTABLISH_INTRO cell. */
+void
+hs_service_intro_has_opened(origin_circuit_t *circuit)
+{
+  int retval = -1;
+
+  /* Check that this circuit is a server-side intro circuit */
+  tor_assert(circuit->base_.purpose == CIRCUIT_PURPOSE_S_ESTABLISH_INTRO);
+  rend_service_t *service = rend_service_get_by_pk_digest(
+                circuit->rend_data->rend_pk_digest);
+  if (!service) {
+    log_warn(LD_REND, "Unrecognized service ID.");
+    return circuit_mark_for_close(TO_CIRCUIT(circuit), END_CIRC_REASON_NOSUCHSERVICE);
+  }
+
+  /* Make sure this circuit is still needed. */
+  /* XXX audit wtf */
+  /* XXX See what other checks the old function was doing. */
+  if ((count_intro_point_circuits(service) -
+       smartlist_len(service->expiring_nodes)) >
+      service->n_intro_points_wanted) {
+    /* Remove the intro point associated with this circuit, it's being
+     * repurposed or closed thus cleanup memory. */
+    rend_intro_point_t *intro = find_intro_point(circuit);
+    if (intro != NULL) {
+      smartlist_remove(service->intro_nodes, intro);
+      rend_intro_point_free(intro);
+    }
+
+    /* XXX lol wtf */
+    if (get_options()->ExcludeNodes) {
+      circuit_mark_for_close(TO_CIRCUIT(circuit), END_CIRC_REASON_NONE);
+    } else {
+      tor_assert(circuit->build_state->is_internal);
+      circuit_change_purpose(TO_CIRCUIT(circuit), CIRCUIT_PURPOSE_C_GENERAL);
+      {
+        rend_data_t *rend_data = circuit->rend_data;
+        circuit->rend_data = NULL;
+        rend_data_free(rend_data);
+      }
+      {
+        crypto_pk_t *intro_key = circuit->intro_key;
+        circuit->intro_key = NULL;
+        crypto_pk_free(intro_key);
+      }
+
+      circuit_has_opened(circuit);
+    }
+    return;
+  }
+
+  retval = send_establish_intro_cell(circuit);
+  if (retval < 0) {
+    log_warn(LD_BUG, "XXX");
+    return circuit_mark_for_close(TO_CIRCUIT(circuit), END_CIRC_REASON_INTERNAL);
+  }
+
   /* We've attempted to use this circuit */
   pathbias_count_use_attempt(circuit);
 }
 
-#endif
