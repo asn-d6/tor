e2fbe8c5ece81bf46c66eb86b884af6cda655c2e
George Kadianakis <desnacked@riseup.net>
2016-07-11 18:35:13 +0300

    Move intro_has_opened() function to hs_service.c

---
+++ src/or/hs_service.h
@@ -0,0 +1,18 @@
+/* Copyright (c) 2001 Matej Pfajfar.
+ * Copyright (c) 2001-2004, Roger Dingledine.
+ * Copyright (c) 2004-2006, Roger Dingledine, Nick Mathewson.
+ * Copyright (c) 2007-2016, The Tor Project, Inc. */
+/* See LICENSE for licensing information */
+
+/**
+ * \file hs_service.h
+ * \brief Header file for hs_service.c.
+ **/
+
+#ifndef TOR_HSSERVICE_H
+#define TOR_HSSERVICE_H
+
+void hs_service_intro_has_opened(origin_circuit_t *circuit);
+
+#endif
+
