bf6785e7bfbdba3694e1e255d2700cd89fcc9425
George Kadianakis <desnacked@riseup.net>
2016-10-26 14:33:35 -0400

    fixup! prop224: Add code that generates ESTABLISH_INTRO cells.

--- src/or/hs_common.h
+++ src/or/hs_common.h
@@ -1,7 +1,7 @@
 /* XXX Merge this with the hs_common.h from #17238 */
 
 /* ed25519 authentication key on ESTABLISH_INTRO cell. */
-#define AUTH_KEY_ED25519 2
+#define AUTH_KEY_ED25519 0x02
 
 /* String prefix for the signature of ESTABLISH_INTRO */
 #define ESTABLISH_INTRO_SIG_PREFIX "Tor establish-intro cell v1"
