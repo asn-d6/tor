104d40f3113fa9e59ce619a057082c4424a6b15c
George Kadianakis <desnacked@riseup.net>
2017-06-01 14:12:33 +0300

    prop224 prep: Add code that launches v3 HS desc fetches.
    Entry point is hs_client_refetch_v3_renddesc().
    
    Will be used in subsequent commits.

--- src/or/hs_client.h
+++ src/or/hs_client.h
@@ -12,5 +12,10 @@
 void hs_client_note_connection_attempt_succeeded(
                                        const edge_connection_t *conn);
 
+typedef struct hs_descriptor_t hs_descriptor_t;
+int hs_any_intro_points_usable(const hs_descriptor_t *desc);
+int
+hs_client_refetch_v3_renddesc(const ed25519_public_key_t *onion_identity_pk);
+
 #endif /* TOR_HS_CLIENT_H */
 
