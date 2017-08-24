e4ded3ccf8d4dc3cfdfd2f174ada53129f17a425
David Goulet <dgoulet@torproject.org>
2017-08-24 11:42:05 -0400

    fixup! prop224: Register RP circuit when it opens

--- src/or/hs_circuitmap.c
+++ src/or/hs_circuitmap.c
@@ -407,26 +407,16 @@ hs_circuitmap_get_rend_circ_service_side(const uint8_t *cookie)
 }
 
 /* Public function: Return client-side rendezvous circuit with rendezvous
- * <b>cookie</b>. It will first lookup for the CIRCUIT_PURPOSE_C_REND_READY
- * purpose and then try for CIRCUIT_PURPOSE_C_REND_READY_INTRO_ACKED.
+ * <b>cookie</b>. It looks up circuit of purpose
+ * CIRCUIT_PURPOSE_C_ESTABLISH_REND.
  *
  * Return NULL if no such circuit is found in the circuitmap. */
 origin_circuit_t *
 hs_circuitmap_get_rend_circ_client_side(const uint8_t *cookie)
 {
-  origin_circuit_t *circ = NULL;
-
-  circ = hs_circuitmap_get_origin_circuit(HS_TOKEN_REND_CLIENT_SIDE,
+  return hs_circuitmap_get_origin_circuit(HS_TOKEN_REND_CLIENT_SIDE,
                                           REND_TOKEN_LEN, cookie,
-                                          CIRCUIT_PURPOSE_C_REND_READY);
-  if (circ) {
-    return circ;
-  }
-
-  circ = hs_circuitmap_get_origin_circuit(HS_TOKEN_REND_CLIENT_SIDE,
-                                          REND_TOKEN_LEN, cookie,
-                                 CIRCUIT_PURPOSE_C_REND_READY_INTRO_ACKED);
-  return circ;
+                                          CIRCUIT_PURPOSE_C_ESTABLISH_REND);
 }
 
 /**** Public servide-side setters: */
