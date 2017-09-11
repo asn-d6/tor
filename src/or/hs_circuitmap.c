aeebd5dba5e2f1950a5fdfb1c4b2251644b457cc
David Goulet <dgoulet@torproject.org>
2017-09-11 11:06:22 -0400

    hs: Handled REND_JOINED circuit when getting an INTRODUCE_ACK
    Because we can get a RENDEZVOUS2 cell before the INTRODUCE_ACK, we need to
    correctly handle the circuit purpose REND_JOINED that is not change its
    purpose when we get an INTRODUCE_ACK and simply close the intro circuit
    normally.
    
    Fixes #23455
    
    Signed-off-by: David Goulet <dgoulet@torproject.org>

--- src/or/hs_circuitmap.c
+++ src/or/hs_circuitmap.c
@@ -409,7 +409,8 @@ hs_circuitmap_get_rend_circ_service_side(const uint8_t *cookie)
 /* Public function: Return client-side rendezvous circuit with rendezvous
  * <b>cookie</b>. It will first lookup for the CIRCUIT_PURPOSE_C_REND_READY
  * purpose and then try for CIRCUIT_PURPOSE_C_REND_READY_INTRO_ACKED and then
- * finally tries for CIRCUIT_PURPOSE_C_ESTABLISH_REND.
+ * the CIRCUIT_PURPOSE_C_REND_JOINED and finally tries for
+ * CIRCUIT_PURPOSE_C_ESTABLISH_REND.
  *
  * Return NULL if no such circuit is found in the circuitmap. */
 origin_circuit_t *
@@ -427,6 +428,13 @@ hs_circuitmap_get_rend_circ_client_side(const uint8_t *cookie)
   circ = hs_circuitmap_get_origin_circuit(HS_TOKEN_REND_CLIENT_SIDE,
                                           REND_TOKEN_LEN, cookie,
                                  CIRCUIT_PURPOSE_C_REND_READY_INTRO_ACKED);
+  if (circ) {
+    return circ;
+  }
+
+  circ = hs_circuitmap_get_origin_circuit(HS_TOKEN_REND_CLIENT_SIDE,
+                                          REND_TOKEN_LEN, cookie,
+                                          CIRCUIT_PURPOSE_C_REND_JOINED);
   if (circ) {
     return circ;
   }
