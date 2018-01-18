9d75aa3ba21b37a9b79401fc99887197ac387723
David Goulet <dgoulet@torproject.org>
2018-01-18 17:43:52 -0500

    dos: Add client DoS statistics object
    This adds the high level dos_client_stats_t object that contains pointers
    (currently only one) to more specific type of detection statistics.
    
    At this commit, the circuit creation statistic object is added but only tracks
    concurrent connection at this stage.
    
    A new and closed client connection entry point are added which notify the DoS
    subsystem that a client connection address action is needed.
    
    Signed-off-by: David Goulet <dgoulet@torproject.org>

--- src/or/dos.c
+++ src/or/dos.c
@@ -9,6 +9,8 @@
 #define DOS_PRIVATE
 
 #include "or.h"
+#include "config.h"
+#include "geoip.h"
 #include "networkstatus.h"
 
 #include "dos.h"
@@ -30,6 +32,36 @@ static uint32_t dos_cc_circuit_time_rate;
 static uint32_t dos_cc_circuit_max_count;
 static uint32_t dos_cc_defense_type;
 static int32_t dos_cc_defense_time_period;
+
+/* Structure that keeps stats of client connection per-IP. */
+typedef struct cc_client_stats_t {
+  /* Concurrent connection count from the specific address. 2^32 is most
+   * likely way to big for the amount of allowed file descriptors. */
+  uint32_t concurrent_count;
+} cc_client_stats_t;
+
+/*
+ * General interface of the denial of service mitigation subsystem.
+ */
+
+/* This object is a top level object that contains everything related to the
+ * per-IP client DoS mitigation. Because it is per-IP, is it used in the geoip
+ * clientmap_entry_t object and opaque to that subsystem. */
+typedef struct dos_client_stats_t {
+  /* Circuit creation statistics. This is set only if the circuit creation
+   * subsystem has been enabled (dos_cc_enabled). */
+  cc_client_stats_t *cc_stats;
+} dos_client_stats_t;
+
+/* Free a circuit creation client connection object. */
+static void
+cc_client_stats_free(cc_client_stats_t *obj)
+{
+  if (obj == NULL) {
+    return;
+  }
+  tor_free(obj);
+}
 
 /* Return true iff the circuit creation mitigation is enabled. We look at the
  * consensus for this else a default value is returned. */
@@ -148,7 +180,151 @@ cc_consensus_has_changed(void)
   return;
 }
 
+/* Called when a new client connection has been established. Allocate the
+ * circuit creation statistics object if needed in the stats object. The
+ * address addr is for logging purposes only. */
+static void
+cc_new_client_conn(const tor_addr_t *addr, dos_client_stats_t *stats)
+{
+  tor_assert(addr);
+  tor_assert(stats);
+
+  if (stats->cc_stats == NULL) {
+    stats->cc_stats = tor_malloc_zero(sizeof(cc_client_stats_t));
+  }
+  stats->cc_stats->concurrent_count++;
+
+  log_debug(LD_DOS, "Client address %s has now %u concurrent connections.",
+            fmt_addr(addr), stats->cc_stats->concurrent_count);
+}
+
+/* Called when a new client connection has been established. Allocate the
+ * circuit creation statistics object if needed in the stats object. The
+ * address addr is for logging purposes only. */
+static void
+cc_close_client_conn(const tor_addr_t *addr, dos_client_stats_t *stats)
+{
+  tor_assert(addr);
+  tor_assert(stats);
+
+  /* Nothing to do here. */
+  if (stats->cc_stats == NULL) {
+    goto end;
+  }
+
+  /* Because the new client connection is noted when the channel becomes open,
+   * this means we can end up here if the connection was closed before it was
+   * ever opened leading to having this down to 0. Just ignore. */
+  if (stats->cc_stats->concurrent_count == 0) {
+    goto end;
+  }
+
+  stats->cc_stats->concurrent_count--;
+  log_debug(LD_DOS, "Client address %s has lost a connection. Concurrent "
+                    "connections are now at %u",
+            fmt_addr(addr), stats->cc_stats->concurrent_count);
+
+ end:
+  return;
+}
+
+/* General private API */
+
+/* Return true iff we have at least one DoS detection enabled. This is used to
+ * decide if we need to allocate any kind of high level DoS object. */
+static inline int
+dos_is_enabled(void)
+{
+  return !!dos_cc_enabled;
+}
+
 /* General API */
+
+/* Called when a new client connection has been established on the given
+ * address. */
+void
+dos_new_client_conn(const tor_addr_t *addr)
+{
+  clientmap_entry_t *entry;
+
+  tor_assert(addr);
+
+  /* Past that point, we know we have at least one DoS detection subsystem
+   * enabled so we'll start allocating stuff. */
+  if (!dos_is_enabled()) {
+    goto end;
+  }
+
+  /* We are only interested in client connection from the geoip cache. */
+  entry = geoip_lookup_client(addr, NULL, GEOIP_CLIENT_CONNECT);
+  if (BUG(entry == NULL)) {
+    /* Should never happen because we note down the address in the geoip
+     * cache before this is called. */
+    goto end;
+  }
+
+  /* It might be the first time we see this address so allocate a DoS client
+   * address statistics object. */
+  if (entry->dos_stats == NULL) {
+    entry->dos_stats = tor_malloc_zero(sizeof(dos_client_stats_t));
+  }
+
+  /* If we have the circuit creation detection enabled, notify it. */
+  if (dos_cc_enabled) {
+    cc_new_client_conn(addr, entry->dos_stats);
+  }
+
+ end:
+  return;
+}
+
+/* Called when a client connection for the given IP address has been closed. */
+void
+dos_close_client_conn(const tor_addr_t *addr)
+{
+  clientmap_entry_t *entry;
+
+  tor_assert(addr);
+
+  /* Past that point, we know we have at least one DoS detection subsystem so
+   * we should lookup anything related to this address. */
+  if (!dos_is_enabled()) {
+    goto end;
+  }
+
+  /* We are only interested in client connection from the geoip cache. */
+  entry = geoip_lookup_client(addr, NULL, GEOIP_CLIENT_CONNECT);
+  if (entry == NULL) {
+    /* This can happen because we can close a connection before the channel
+     * got to be noted down in the geoip cache. */
+    goto end;
+  }
+
+  /* This could happen if we free a connection object before the channel was
+   * ever opened. */
+  if (entry->dos_stats == NULL) {
+    goto end;
+  }
+
+  /* If we have the circuit creation detection enabled, notify it. */
+  if (dos_cc_enabled) {
+    cc_close_client_conn(addr, entry->dos_stats);
+  }
+
+ end:
+  return;
+}
+
+/* Free the given dos_client_stats_t object. */
+void
+dos_client_stats_free(dos_client_stats_t *obj)
+{
+  if (obj == NULL) {
+    return;
+  }
+  cc_client_stats_free(obj->cc_stats);
+  tor_free(obj);
+}
 
 /* Called when a the consensus has changed. We might have new consensus
  * parameters to look at. */
