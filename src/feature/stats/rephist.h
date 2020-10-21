b04852d945452cf49a48c1842adb0ccb8c35c57d
George Kadianakis <desnacked@riseup.net>
2020-10-21 16:43:39 +0300

    Abstract v2/v3 stats write function into a single function.

--- src/feature/stats/rephist.h
+++ src/feature/stats/rephist.h
@@ -79,13 +79,12 @@ void rep_hist_log_circuit_handshake_stats(time_t now);
 
 void rep_hist_hs_stats_init(time_t now);
 void rep_hist_hs_stats_term(void);
+time_t rep_hist_hs_stats_write(time_t now, bool is_v3);
 
-time_t rep_hist_hs_v2_stats_write(time_t now);
 char *rep_hist_get_hs_v2_stats_string(void);
 void rep_hist_seen_new_rp_cell(bool is_v2);
 void rep_hist_hsdir_stored_maybe_new_v2_onion(const crypto_pk_t *pubkey);
 
-time_t rep_hist_hs_v3_stats_write(time_t now);
 char *rep_hist_get_hs_v3_stats_string(void);
 void rep_hist_hsdir_stored_maybe_new_v3_onion(const uint8_t *blinded_key);
 
