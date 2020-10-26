98301f58a954b1f58ef7c49050b90f168e7f0e91
George Kadianakis <desnacked@riseup.net>
2020-10-26 18:27:16 +0200

    Make room for v3: Complete move from hs_stats_t to hs_v2_stats_t.

--- src/feature/stats/rephist.h
+++ src/feature/stats/rephist.h
@@ -64,10 +64,9 @@ MOCK_DECL(int, rep_hist_get_circuit_handshake_requested, (uint16_t type));
 MOCK_DECL(int, rep_hist_get_circuit_handshake_assigned, (uint16_t type));
 
 void rep_hist_hs_v2_stats_init(time_t now);
-void rep_hist_hs_v2_stats_term(void);
 time_t rep_hist_hs_v2_stats_write(time_t now);
 char *rep_hist_get_hs_v2_stats_string(void);
-void rep_hist_seen_new_rp_cell(void);
+void rep_hist_seen_new_rp_cell(bool is_v2);
 void rep_hist_hsdir_stored_maybe_new_v2_onion(const crypto_pk_t *pubkey);
 void rep_hist_hsdir_stored_maybe_new_v3_onion(const uint8_t *blinded_key);
 
@@ -83,6 +82,25 @@ extern uint32_t rephist_total_num;
 extern int onion_handshakes_requested[MAX_ONION_HANDSHAKE_TYPE+1];
 extern int onion_handshakes_assigned[MAX_ONION_HANDSHAKE_TYPE+1];
 #endif
+
+#ifdef REPHIST_PRIVATE
+/** Carries the various hidden service statistics, and any other
+ *  information needed. */
+typedef struct hs_v2_stats_t {
+  /** How many v2 relay cells have we seen as rendezvous points? */
+  uint64_t rp_v2_relay_cells_seen;
+
+  /** Set of unique public key digests we've seen this stat period
+   * (could also be implemented as sorted smartlist). */
+  digestmap_t *v2_onions_seen_this_period;
+} hs_v2_stats_t;
+
+typedef struct bw_array_t bw_array_t;
+STATIC uint64_t find_largest_max(bw_array_t *b);
+STATIC void commit_max(bw_array_t *b);
+STATIC void advance_obs(bw_array_t *b);
+STATIC char *rep_hist_format_hs_v2_stats(time_t now);
+#endif /* defined(REPHIST_PRIVATE) */
 
 /**
  * Represents the type of a cell for padding accounting
@@ -108,5 +126,10 @@ char *rep_hist_get_padding_count_lines(void);
 void rep_hist_reset_padding_counts(void);
 void rep_hist_prep_published_padding_counts(time_t now);
 void rep_hist_padding_count_timers(uint64_t num_timers);
+
+#ifdef TOR_UNIT_TESTS
+typedef struct hs_v2_stats_t hs_v2_stats_t;
+const hs_v2_stats_t *rep_hist_get_hs_v2_stats(void);
+#endif
 
 #endif /* !defined(TOR_REPHIST_H) */
